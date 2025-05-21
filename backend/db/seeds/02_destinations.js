df2162e698bd064a167f32e4f2aeceaf7bc802c9
Abdullah Zia <amz1cflp99@gmail.com>
2025-05-21 18:49:50 -0400

    added user data for destinations and flights

---
+++ backend/db/seeds/02_destinations.js
@@ -0,0 +1,58 @@
+// code for destinations
+
+
+
+
+exports.seed = async function(knex) {
+  // Will delete all existing entries
+  await knex('destinations').del();
+  
+  // Insert seed entries
+  await knex('destinations').insert([
+    {
+      id: 1,
+      city: 'Paris',
+      country: 'France',
+      description: 'The city of lights and love.',
+      deal: '20% off flights',
+      lat: 48.8566,
+      lng: 2.3522
+    },
+    {
+      id: 2,
+      city: 'Tokyo',
+      country: 'Japan',
+      description: 'A bustling hub of culture and technology.',
+      deal: '15% off hotels',
+      lat: 35.6895,
+      lng: 139.6917
+    },
+    {
+      id: 3,
+      city: 'New York',
+      country: 'USA',
+      description: 'The city that never sleeps.',
+      deal: '10% off attractions',
+      lat: 40.7128,
+      lng: -74.0060
+    },
+    {
+      id: 4,
+      city: 'Hong Kong',
+      country: 'China',
+      description: 'Visit Hong Kong',
+      deal: '25% off flights',
+      lat: 22.3193,
+      lng: 114.1694
+    },
+    {
+      id: 5,
+      city: 'Toronto',
+      country: 'Canada',
+      description: 'Visit Toronto.',
+      deal: '30% off flights',
+      lat: 43.651070,
+      lng: -79.347015
+    }
+  ]);
+};
